c10a4eb071166a05f8ae5a0da001533a52994fc2
MstfaZahr <61924314+MstfaZahr@users.noreply.github.com>
2025-11-09 14:55:02 +0200

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 3,          // عدد الصور اللي تظهر في نفس الوقت
-   spaceBetween: 30,          // المسافة بين الصور
-   loop: true,                // تشغيل التكرار
-   autoplay: {
-      delay: 3000,             // الوقت بين كل تبديل (3 ثواني)
-      disableOnInteraction: false,
-   },
-   navigation: {
-      nextEl: ".custom-button-next",
-      prevEl: ".custom-button-prev",
-   },
-   pagination: {
-      el: ".swiper-pagination", // النقاط تحت السلايدر (اختياري)
-      clickable: true,
-   },
-   breakpoints: {
-      768: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 },
-      480: { slidesPerView: 1 },
-   },
-});
+// var swiper = new Swiper(".mySwiper", {
+//    slidesPerView: 3,          // عدد الصور اللي تظهر في نفس الوقت
+//    spaceBetween: 30,          // المسافة بين الصور
+//    loop: true,                // تشغيل التكرار
+//    autoplay: {
+//       delay: 3000,             // الوقت بين كل تبديل (3 ثواني)
+//       disableOnInteraction: false,
+//    },
+//    navigation: {
+//       nextEl: ".custom-button-next",
+//       prevEl: ".custom-button-prev",
+//    },
+//    pagination: {
+//       el: ".swiper-pagination", // النقاط تحت السلايدر (اختياري)
+//       clickable: true,
+//    },
+//    breakpoints: {
+//       768: { slidesPerView: 2 },
+//       1024: { slidesPerView: 3 },
+//       480: { slidesPerView: 1 },
+//    },
+// });
 
 const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
 if (isTouchDevice) {
-   const elements = document.querySelectorAll('.service');
+   const services = document.querySelectorAll('.service');
+   const profiles = document.querySelectorAll('.profile-card');
 
    const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
@@ -36,5 +37,6 @@ if (isTouchDevice) {
       });
    }, { threshold: 1 }); // يبدأ لما 30٪ من العنصر تبقى ظاهرة
 
-   elements.forEach(el => observer.observe(el));
+   services.forEach(el => observer.observe(el));
+   profiles.forEach(el => observer.observe(el));
 }
